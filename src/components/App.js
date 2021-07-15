b867223718c25b3574eee66db041fc75599cf646
jihye kang <jihye59kang@gmail.com>
2021-07-16 00:35:02 +0100

    use Link tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
     return (
         <div>
             PageOne
+            <Link to="/pagetwo">PageTwo</Link>
         </div>
     )
 }
@@ -13,6 +14,7 @@ const PageTwo = () => {
     return (
         <div>
             PageTwo
+            <Link to="/">PageOne</Link>
         </div>
     )
 }
