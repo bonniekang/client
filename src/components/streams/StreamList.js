b4f926462bf1856bc99275f00eede654ff04db63
jihye kang <jihye59kang@gmail.com>
2021-07-19 00:53:49 +0100

    showing edit/ delete

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,12 +8,19 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return <div>Edit/ Delete</div>
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div key={stream.id}>
                     {stream.title}
                     {stream.description}
+                    {this.renderAdmin(stream)}
                 </div>
             )
         })
@@ -29,7 +36,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) }
+    return { 
+        streams: Object.values(state.streams) ,
+        currentUserId: state.auth.userId
+    }
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
