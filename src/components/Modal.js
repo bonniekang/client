40593588882bafb31f957b0a5525e4940f0ac80f
jihye kang <jihye59kang@gmail.com>
2021-07-19 20:19:14 +0100

    close modal event

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history'
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="modals">
-            <div className="modal">
-                modal
+        <div onClick={() => history.push('/')} className="modals">
+            <div onClick={(e) => e.stopPropagation()} 
+            className="modal">
+                are you sure you want to delete this?
             </div>
         </div>,
         document.querySelector('#modal')
