4cda5997c4ca34ba9a293ced6d49666501a43b22
jihye kang <jihye59kang@gmail.com>
2021-07-19 01:00:58 +0100

    rendercreate link btn

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 import { fetchStreams } from '../../actions'
 
 class StreamList extends React.Component {
@@ -26,10 +27,23 @@ class StreamList extends React.Component {
         })
     }
 
+    renderCreate(){
+        if(this.props.isSignedIn){
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" >
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
                 {this.renderList()}
+                {this.renderCreate()}
             </div>
         )
     }
@@ -38,7 +52,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     return { 
         streams: Object.values(state.streams) ,
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
